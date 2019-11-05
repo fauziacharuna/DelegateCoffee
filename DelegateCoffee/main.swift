7fe9dc5a8c87422d549c84c9559cc0d0bc7ad04a
fauziachmadharuna <fauziharuna@gmail.com>
2019-11-05 12:52:56 +0700

    Initial Commit

---
+++ DelegateCoffee/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DelegateCoffee
+//
+//  Created by Fauzi Achmad Haruna on 05/11/19.
+//  Copyright © 2019 Fauzi Achmad Haruna. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
